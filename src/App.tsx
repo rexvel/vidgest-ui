ffca50e25627ca92aea1c0bc2e149c1ff7d67f3f
rexvel <rexvelele@gmail.com>
2024-08-19 18:00:35 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import VideoTakewaysList from './components/VideoTakewaysList';
 import useMindTreeData from './hooks'
-import './App.css'
 import VideoUrlForm from './components/PromptInput';
+import './App.css'
 
 
 function App() {
