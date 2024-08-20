162490a2375d764664a591f25920c595bb2281c9
rexvel <rexvelele@gmail.com>
2024-08-20 15:28:06 +0300

    feat: add hook; display data for mocked video

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import VideoTakewaysList from './components/VideoTakewaysList';
 import useMindTreeData from './hooks'
 import VideoUrlForm from './components/PromptInput';
 import './App.css'
+import { useYouTubeVideoInfo } from './hooks/useYoutubeVideoInfo';
+import YouTubeVideoCard from './components/YoutubeVideoCard';
 
 
 function App() {
   const { data, loading, error, fetchData } = useMindTreeData();
 
+  // const youtubeData = useYouTubeVideoInfo("eywYgAf27rs");
+  // console.log(youtubeData);
   return (
     <div className="p-4 max-w-4xl mx-auto">
       <VideoUrlForm onSubmit={fetchData} />
-
+      <YouTubeVideoCard videoId='eywYgAf27rs' />
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
       {data && <VideoTakewaysList data={data}/> }
